fda02b4e7998ce7c0ff897ab62f235d0ab6bfd7e
Phi064 <100498908+Phi064@users.noreply.github.com>
2024-03-04 15:22:39 +0100

    Delete Lesson 1 directory

--- Lesson 1/einstein.py
+++
@@ -1,5 +0,0 @@
-# Implement a program in Python that prompts the user for mass as an integer
-mass = int(input("Introduce a mass in kilograms: "))
-
-# And then outputs the equivalent number of Joules
-print("E:", mass * 300000000**2)
--- Lesson 1/playbackspeed.py
+++
@@ -1,5 +0,0 @@
-# Take user input
-phrase = input("What do you want to say? ").replace(" ", "...")
-
-# Replace the whitespaces with dots
-print(phrase)
