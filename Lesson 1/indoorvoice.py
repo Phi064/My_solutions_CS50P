1cc3d5ab25eadc33543be430f76b96ef9f8096eb
Phi064 <100498908+Phi064@users.noreply.github.com>
2024-03-04 15:15:55 +0100

    Add files via upload

---
+++ Lesson 1/indoorvoice.py
@@ -0,0 +1,7 @@
+# Create a program that takes an input and outputs it in lowercase
+
+# Take the input and lowercase it
+voice = input("What do you want to say? ").lower()
+
+# Print it
+print(voice)
